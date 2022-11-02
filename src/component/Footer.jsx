1d869704a2e156c11b3123113fb147a46e2775c3
iamclement1 <blessedmarcel1@gmail.com>
2022-11-03 00:11:30 +0100

    feats: completed footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,16 @@
+import Zuri from '../assets/zuri.png';
+import I4g from '../assets/I4G.png';
 export default function Footer() {
     return (
         <>
-        <footer>
+        
+        <footer className="md:flex justify-between w-5/6 mx-auto mt-6">
             
+            <img src={Zuri} alt="zuri__logo" className="h-4" />
+            <span className="text-sm text-gray-500">
+                HNG Internship 9 Frontend Task
+            </span>
+            <img src={I4g} alt="i4g__logo" className="h-4" />
         </footer>
         </>
     )
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import Profile from '../assets/profile.jpeg'
 import Slack from '../assets/slack.png';
 import Github from '../assets/github.png';
+import Footer from '../component/Footer';
 
 export default function Landing() {
     return (
@@ -14,32 +15,32 @@ export default function Landing() {
             </div>
 
             <div className="mt-12 text-sm">
-                <div className="bg-gray-300 p-4 w-3/6 mx-auto text-center mb-6">
+                <div className="bg-gray-300 p-4 md:w-3/6 w-5/6 rounded-md mx-auto text-center mb-6">
                     <span className="cursor-pointer" onClick={() => window.location.href = ""}>
                         Twitter Link
                     </span>
                 </div>
-                <div className="bg-gray-300 p-4 w-3/6 mx-auto text-center mb-6">
+                <div className="bg-gray-300 p-4 md:w-3/6 w-5/6 rounded-md mx-auto text-center mb-6">
                     <span className="cursor-pointer " onClick={() => window.location.href = "https://training.zuri.team/"}>
                         Zuri Team
                     </span>
                 </div>
-                <div className="bg-gray-300 p-4 w-3/6 mx-auto text-center mb-6">
+                <div className="bg-gray-300 p-4 md:w-3/6 w-5/6 rounded-md mx-auto text-center mb-6">
                     <span className="cursor-pointer " onClick={() => window.location.href = "https://books.zuri.team/"}>
                         Zuri Books
                     </span>
                 </div>
-                <div className="bg-gray-300 p-4 w-3/6 mx-auto text-center mb-6">
+                <div className="bg-gray-300 p-4 md:w-3/6 w-5/6 rounded-md mx-auto text-center mb-6">
                     <span className="cursor-pointer " onClick={() => window.location.href = "https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E"}>
                         Python Books
                     </span>
                 </div>
-                <div className="bg-gray-300 p-4 w-3/6 mx-auto text-center mb-6 ">
+                <div className="bg-gray-300 p-4 md:w-3/6 w-5/6 rounded-md mx-auto text-center mb-6 ">
                     <span className="cursor-pointer " onClick={() => window.location.href = "https://background.zuri.team/"}>
                         Background Check for Coders
                     </span>
                 </div>
-                <div className="bg-gray-300 p-4 w-3/6 mx-auto text-center mb-2">
+                <div className="bg-gray-300 p-4 md:w-3/6 w-5/6 rounded-md mx-auto text-center mb-2">
                     <span className="cursor-pointer " onClick={() => window.location.href = "https://books.zuri.team/design-rules"}>
                         Design Books
                     </span>
@@ -50,6 +51,10 @@ export default function Landing() {
                 </div>
 
             </div>
+            <div className="w-5/6 mx-auto mt-20">
+            <hr  className="text-center"/>
+            </div>
+            <Footer />
         </section>
         </>
     )
